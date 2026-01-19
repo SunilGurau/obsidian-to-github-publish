1024565063a3c1c518e8370c0a5da642c5dc3a09
Sunil Gurau <075bei043.sunil@pcampus.edu.np>
2026-01-19 10:55:05 +0545

    add settings to settings tab

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,21 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
 import MyPlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface PublishPluginSettings {
+	github_pat: string;
+  owner: string;
+  repo: string;
+  directory: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export const DEFAULT_SETTINGS: PublishPluginSettings = {
+	github_pat: '',
+  owner: '',
+  repo: '',
+  directory: ''
 }
 
-export class SampleSettingTab extends PluginSettingTab {
+export class PublishSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -23,14 +29,49 @@ export class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
+			.setName('Repository')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your repo name')
+				.setValue(this.plugin.settings.github_pat)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.github_pat = value;
 					await this.plugin.saveSettings();
 				}));
+
+    new Setting(containerEl)  
+    .setName('Directory')    
+    .addText((text) =>  
+      text  
+          .setPlaceholder('Enter directory path')  
+          .setValue(this.plugin.settings.directory)  
+          .onChange(async (value) => {  
+            this.plugin.settings.directory = value;
+            await this.plugin.saveSettings();
+          })
+    );
+
+    new Setting(containerEl)  
+    .setName('Owner')    
+    .addText((text) =>  
+      text  
+          .setPlaceholder('Enter repository owner')  
+          .setValue(this.plugin.settings.owner)  
+          .onChange(async (value) => {  
+            this.plugin.settings.owner = value;
+            await this.plugin.saveSettings();
+          })
+    );
+    new Setting(containerEl)  
+    .setName("Personal access token")
+    .addText((text) =>  
+      text  
+          .setPlaceholder('Enter your personal access token')  
+          .setValue(this.plugin.settings.github_pat)  
+          .onChange(async (value) => {  
+            this.plugin.settings.github_pat = value;
+            await this.plugin.saveSettings();
+          })
+    );
+
 	}
 }
