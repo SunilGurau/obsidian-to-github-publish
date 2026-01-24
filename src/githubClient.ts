04a62e63174ef7654cb5dcf36455c99b2fe37ef7
SunilGurau <sunil137.gurau@gmail.com>
2026-01-25 02:11:41 +0545

    register a handler for file modification, implemented with debouncing

--- src/githubClient.ts
+++ src/githubClient.ts
@@ -1,110 +1,114 @@
-import { Octokit } from '@octokit/core';
+import { Octokit } from "@octokit/core";
 
-interface Test{
-  foo: string;
+interface Test {
+	foo: string;
 }
 export default class GithubConnector {
-  private github_pat: string;
-  private owner: string;
-  private repo: string;
-  private path: string;
-  private octokit: Octokit;
-  private fooTest: Test;
+	private github_pat: string;
+	private owner: string;
+	private repo: string;
+	private path: string;
+	private octokit: Octokit;
+	private fooTest: Test;
 
-  constructor(github_pat: string, owner: string, repo: string, path: string) {
-    this.github_pat = github_pat;
-    this.owner = owner;
-    this.repo = repo;
-    this.path = path
-    this.octokit = new Octokit({
-      auth: this.github_pat,
-    });
-  }
+	constructor(github_pat: string, owner: string, repo: string, path: string) {
+		this.github_pat = github_pat;
+		this.owner = owner;
+		this.repo = repo;
+		this.path = path;
+		this.octokit = new Octokit({
+			auth: this.github_pat,
+		});
+	}
 
-  private async getFileSha() {
-  const owner = this.owner;
-  const repo = this.repo;
-  const path = this.path;
+	private async getFileSha() {
+		const owner = this.owner;
+		const repo = this.repo;
+		const path = this.path;
 
-  const response = await this.octokit.request(
-    "GET /repos/{owner}/{repo}/contents/{path}",
-    { owner, repo, path }
-  );
+		const response = await this.octokit.request(
+			"GET /repos/{owner}/{repo}/contents/{path}",
+			{ owner, repo, path },
+		);
 
-  // console.log(response.data.sha);
-  return response.data.sha;
+		// console.log(response.data.sha);
+		return response.data.sha;
+	}
+	async getFile() {
+		const res = await this.octokit.request(
+			"GET /repos/{owner}/{repo}/contents/{path}",
+			{
+				owner: this.owner,
+				repo: this.repo,
+				path: this.path,
+				headers: {
+					"X-GitHub-Api-Version": "2022-11-28",
+				},
+			},
+		);
+		return res;
+	}
+
+	async createFile() {
+		const res = await this.octokit.request(
+			"PUT /repos/{owner}/{repo}/contents/{path}",
+			{
+				owner: this.owner,
+				repo: this.repo,
+				path: this.path,
+				message: "obsidian note added",
+				committer: {
+					name: "name here",
+					email: "email here",
+				},
+				content: "bXkgbmV3IGZpbGUgY29udGVudHM=",
+				headers: {
+					"X-GitHub-Api-Version": "2022-11-28",
+				},
+			},
+		);
+	}
+
+	async updateFile() {
+		const fileSha = await this.getFileSha();
+		console.debug("File SHA", fileSha);
+		const res = await this.octokit.request(
+			"PUT /repos/{owner}/{repo}/contents/{path}",
+			{
+				owner: this.owner,
+				repo: this.repo,
+				path: this.path,
+				message: "obsidian note updated",
+				committer: {
+					name: "name here",
+					email: "email here",
+				},
+				content: "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz",
+				sha: fileSha,
+				headers: {
+					"X-GitHub-Api-Version": "2022-11-28",
+				},
+			},
+		);
+	}
 }
-  async getFile()
-  {
-    const res = await this.octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-  owner: this.owner,
-  repo: this.repo,
-  path: this.path,
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
-    return res;
-  }
 
-  async createFile()
-  {
-    const res = await this.octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-  owner: this.owner,
-  repo: this.repo,
-  path: this.path,
-  message: 'obsidian note added',
-  committer: {
-    name: 'name here',
-    email: 'email here'
-  },
-  content: 'bXkgbmV3IGZpbGUgY29udGVudHM=',
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
-  }
-
-  async updateFile()
-  {
-    const fileSha = await this.getFileSha();
-    console.debug("File SHA", fileSha);
-    const res = await this.octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-      owner: this.owner,
-      repo: this.repo,
-      path: this.path,
-      message: 'obsidian note updated',
-      committer: {
-        name: 'name here',
-        email: 'email here'
-      },
-      content: 'bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz',
-      sha: fileSha,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    })
-    
-  }
-}
-
-function main(){
-
-  const connector = new GithubConnector(
-    "github_pat_here",
-    "SunilGurau",
-    "portfolio-website",
-    // "test.txt")
-    "test.txt")
-  connector.updateFile().then(res => {
-    // console.debug(res); // actual file info
-  }).catch(err => {
-    console.error(err);
-  });
-
+function main() {
+	const connector = new GithubConnector(
+		"github_pat_here",
+		"SunilGurau",
+		"portfolio-website",
+		// "test.txt")
+		"test.txt",
+	);
+	connector
+		.updateFile()
+		.then((res) => {
+			// console.debug(res); // actual file info
+		})
+		.catch((err) => {
+			console.error(err);
+		});
 }
 
 main();
-
-
-
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,50 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, requestUrl, RequestUrlParam} from 'obsidian';
-import {DEFAULT_SETTINGS, PublishPluginSettings, PublishSettingTab} from "./settings";
+import { debounce, Plugin, TAbstractFile, TFile } from "obsidian";
+import {
+	DEFAULT_SETTINGS,
+	PublishPluginSettings,
+	PublishSettingTab,
+} from "./settings";
+
+// import { debounce } from "obsidian";
 
 export default class PublishPlugin extends Plugin {
 	settings: PublishPluginSettings;
-  // console.log(settings.repo)
+	// console.log(settings.repo)
 
 	async onload() {
 		await this.loadSettings();
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new PublishSettingTab(this.app, this));
-    
+
+		//acts as a decorator to limit the rate of function calls
+		//takes a function and returns a new function that can only be called once every X milliseconds
+		const onModify = debounce(
+			async (file: TFile) => {
+				const content = await this.app.vault.read(file);
+				console.log("Debounced modify:", file.path);
+				console.log("Content:", content);
+			},
+			5000,
+			true,
+		);
+
+		this.registerEvent(
+			this.app.vault.on("modify", (file: TAbstractFile) => {
+				if (file instanceof TFile) {
+					onModify(file);
+				}
+			}),
+		);
 	}
 
-	onunload() {
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			(await this.loadData()) as Partial<MyPluginSettings>,
+		);
 	}
 
 	async saveSettings() {
