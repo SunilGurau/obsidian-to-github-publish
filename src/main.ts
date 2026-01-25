89c39829ded43036b3d718292fd2c0e023018d1e
SunilGurau <sunil137.gurau@gmail.com>
2026-01-25 19:53:24 +0545

    implement the handler to push on change based on the settings

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,49 @@ import {
 	PublishSettingTab,
 } from "./settings";
 
+import { extname } from "path";
 // import { debounce } from "obsidian";
 
 export default class PublishPlugin extends Plugin {
 	settings: PublishPluginSettings;
 	// console.log(settings.repo)
 
+	private onModifyHandler = async (file: TAbstractFile) => {
+		//if not file a markdown file or push_on_change is false, return
+		console.log("filepath:", file.path);
+		if (file instanceof TFile) {
+			console.log("file extension:", file.extension);
+		}
+		if (
+			!(file instanceof TFile) ||
+			extname(file.path) !== ".md" ||
+			!this.settings.push_on_change //push
+		)
+			return;
+		// Here you would add the logic to push the file to GitHub using your GithubConnector
+		console.log("Pushing because push_on_change is enabled");
+		console.log("File Content:", await this.app.vault.read(file));
+	};
+
 	async onload() {
 		await this.loadSettings();
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new PublishSettingTab(this.app, this));
 
-		//acts as a decorator to limit the rate of function calls
-		//takes a function and returns a new function that can only be called once every X milliseconds
-		const onModify = debounce(
-			async (file: TFile) => {
-				const content = await this.app.vault.read(file);
-				console.log("Debounced modify:", file.path);
-				console.log("Content:", content);
-			},
-			5000,
-			true,
+		this.registerEvent(
+			this.app.vault.on(
+				"modify",
+				debounce(this.onModifyHandler, 3000, true),
+			),
 		);
 
-		this.registerEvent(
-			this.app.vault.on("modify", (file: TAbstractFile) => {
-				if (file instanceof TFile) {
-					onModify(file);
-				}
-			}),
-		);
+		this.addCommand({
+			id: "my-plugin-do-something",
+			name: "Do something cool",
+			callback: () => {
+				console.log("Doing something cool!");
+			},
+		});
 	}
 
 	onunload() {}
@@ -43,7 +56,7 @@ export default class PublishPlugin extends Plugin {
 		this.settings = Object.assign(
 			{},
 			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<MyPluginSettings>,
+			(await this.loadData()) as Partial<PublishPluginSettings>,
 		);
 	}
 
--- src/settings.ts
+++ src/settings.ts
@@ -1,21 +1,21 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
+import { App, PluginSettingTab, Setting } from "obsidian";
 import MyPlugin from "./main";
 
 export interface PublishPluginSettings {
 	github_pat: string;
-  owner: string;
-  repo: string;
-  directory: string;
-  push_on_change: boolean;
+	owner: string;
+	repo: string;
+	directory: string;
+	push_on_change: boolean;
 }
 
 export const DEFAULT_SETTINGS: PublishPluginSettings = {
-	github_pat: '',
-  owner: '',
-  repo: '',
-  directory: '',
-  push_on_change: false
-}
+	github_pat: "",
+	owner: "",
+	repo: "",
+	directory: "",
+	push_on_change: false,
+};
 
 export class PublishSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -26,69 +26,76 @@ export class PublishSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Repository')
-      .setDesc('GitHub repository name to publish files to.')
-			.addText(text => text
-				.setPlaceholder('Enter your repo name')
-				.setValue(this.plugin.settings.github_pat)
-				.onChange(async (value) => {
-					this.plugin.settings.github_pat = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Repository")
+			.setDesc("GitHub repository name to publish files to.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your repo name")
+					.setValue(this.plugin.settings.github_pat)
+					.onChange(async (value) => {
+						this.plugin.settings.github_pat = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 
-    new Setting(containerEl)  
-    .setName('Directory')
-    .setDesc('Directory in the repository to publish files to.')   
-    .addText((text) =>  
-      text  
-          .setPlaceholder('Enter directory path')  
-          .setValue(this.plugin.settings.directory)  
-          .onChange(async (value) => {  
-            this.plugin.settings.directory = value;
-            await this.plugin.saveSettings();
-          })
-    );
+		new Setting(containerEl)
+			.setName("Directory")
+			.setDesc("Directory in the repository to publish files to.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter directory path")
+					.setValue(this.plugin.settings.directory)
+					.onChange(async (value) => {
+						this.plugin.settings.directory = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 
-    new Setting(containerEl)  
-    .setName('Owner')
-    .setDesc('GitHub repository owner.')    
-    .addText((text) =>  
-      text  
-          .setPlaceholder('Enter repository owner')  
-          .setValue(this.plugin.settings.owner)  
-          .onChange(async (value) => {  
-            this.plugin.settings.owner = value;
-            await this.plugin.saveSettings();
-          })
-    );
-    new Setting(containerEl)  
-    .setName("Personal access token")
-    .setDesc("Personal access token with write access to the repository.")
-    .addText((text) =>  
-      text  
-          .setPlaceholder('Enter your PAT')  
-          .setValue(this.plugin.settings.github_pat)  
-          .onChange(async (value) => {  
-            this.plugin.settings.github_pat = value;
-            await this.plugin.saveSettings();
-          })
-    );
+		new Setting(containerEl)
+			.setName("Owner")
+			.setDesc("GitHub repository owner.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter repository owner")
+					.setValue(this.plugin.settings.owner)
+					.onChange(async (value) => {
+						this.plugin.settings.owner = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("Personal access token")
+			.setDesc(
+				"Personal access token with write access to the repository.",
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your PAT")
+					.setValue(this.plugin.settings.github_pat)
+					.onChange(async (value) => {
+						this.plugin.settings.github_pat = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 
-    new Setting(containerEl)  
-    .setName('Push on change')
-    .setDesc('Automatically push changes to GitHub when a file is modified.')
-    .addToggle(toggle => toggle  
-       .setValue(this.plugin.settings.push_on_change)  
-       .onChange(async (value) => {  
-          this.plugin.settings.push_on_change = value;  
-          await this.plugin.saveSettings();  
-          this.display();  
-       })  
-    );
+		new Setting(containerEl)
+			.setName("Push on change")
+			.setDesc(
+				"Automatically push changes to GitHub when a file is modified.",
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.push_on_change)
+					.onChange(async (value) => {
+						this.plugin.settings.push_on_change = value;
+						await this.plugin.saveSettings();
+						this.display();
+					}),
+			);
 	}
 }
